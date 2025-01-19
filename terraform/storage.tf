d92b81aa4f7ab775a871286e1e048e36a5e39660
Sharayu <sharayu@Sharayus-MacBook-Pro.local>
2025-01-19 15:19:47 -0500

    bucket name changed

--- terraform/storage.tf
+++ terraform/storage.tf
@@ -1,5 +1,5 @@
-resource "google_storage_bucket" "auto-expire" {
-  name          = "auto-expiring-bucket"
+resource "google_storage_bucket" "raw-document-bucket" {
+  name          = "raw-document-bucket"
   location      = "us-central1"
   force_destroy = true
 
