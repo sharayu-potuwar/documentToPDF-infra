5243a293071813be9c34ad6dea0dfc0496809963
Sharayu <sharayu@Sharayus-MacBook-Pro.local>
2025-01-19 16:27:57 -0500

    bucket name changes also new bucket provisioned

--- terraform/storage.tf
+++ terraform/storage.tf
@@ -1,5 +1,20 @@
-resource "google_storage_bucket" "raw-document-bucket" {
-  name          = "raw-document-bucket"
+resource "google_storage_bucket" "raw-doc-bkt" {
+  name          = "raw-doc-bkt"
+  location      = "us-central1"
+  force_destroy = true
+
+  lifecycle_rule {
+    condition {
+      age = 3
+    }
+    action {
+      type = "Delete"
+    }
+  }
+}
+
+resource "google_storage_bucket" "pdf-doc-bkt" {
+  name          = "pdf-doc-bkt"
   location      = "us-central1"
   force_destroy = true
 
