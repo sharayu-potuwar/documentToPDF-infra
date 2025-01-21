77117ab8f8a89ff17abc25ada2af5fa1dc4a08d3
Sharayu <sharayu@Sharayus-MacBook-Pro.local>
2025-01-21 18:11:19 -0500

    artifact registry name modified

--- terraform/artifactrepo.tf
+++ terraform/artifactrepo.tf
@@ -1,6 +1,6 @@
 resource "google_artifact_registry_repository" "my-repo" {
   location      = "us-central1"
-  repository_id = "employee-repository"
+  repository_id = "doctopdf-repository"
   description   = "This repository is created for docker images of employee app and cloudsql auth proxy"
   format        = "DOCKER"
 
