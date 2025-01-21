3b71ce26840ad65a511d88518a56b47fa5b2c5f2
Sharayu <sharayu@Sharayus-MacBook-Pro.local>
2025-01-21 17:28:10 -0500

    removed secret configuration

--- terraform/build.tf
+++ terraform/build.tf
@@ -1,30 +1,30 @@
-// Create a secret containing the personal access token and grant permissions to the Service Agent
-resource "google_secret_manager_secret" "github_token_secret" {
-    project =  "gcp-devops-436118"
-    secret_id = "github-secret"
+# // Create a secret containing the personal access token and grant permissions to the Service Agent
+# resource "google_secret_manager_secret" "github_token_secret" {
+#     project =  "gcp-devops-436118"
+#     secret_id = "github-secret"
 
-    replication {
-        auto {}
-    }
-}
+#     replication {
+#         auto {}
+#     }
+# }
 
-resource "google_secret_manager_secret_version" "github_token_secret_version" {
-    secret = google_secret_manager_secret.github_token_secret.id
-    secret_data = var.secret_data
-}
+# resource "google_secret_manager_secret_version" "github_token_secret_version" {
+#     secret = google_secret_manager_secret.github_token_secret.id
+#     secret_data = var.secret_data
+# }
 
-data "google_iam_policy" "serviceagent_secretAccessor" {
-    binding {
-        role = "roles/secretmanager.secretAccessor"
-        members = ["serviceAccount:service-268852292565@gcp-sa-cloudbuild.iam.gserviceaccount.com"]
-    }
-}
+# data "google_iam_policy" "serviceagent_secretAccessor" {
+#     binding {
+#         role = "roles/secretmanager.secretAccessor"
+#         members = ["serviceAccount:service-268852292565@gcp-sa-cloudbuild.iam.gserviceaccount.com"]
+#     }
+# }
 
-resource "google_secret_manager_secret_iam_policy" "policy" {
-  project = google_secret_manager_secret.github_token_secret.project
-  secret_id = google_secret_manager_secret.github_token_secret.secret_id
-  policy_data = data.google_iam_policy.serviceagent_secretAccessor.policy_data
-}
+# resource "google_secret_manager_secret_iam_policy" "policy" {
+#   project = google_secret_manager_secret.github_token_secret.project
+#   secret_id = google_secret_manager_secret.github_token_secret.secret_id
+#   policy_data = data.google_iam_policy.serviceagent_secretAccessor.policy_data
+# }
 
 // Create the GitHub connection
 resource "google_cloudbuildv2_connection" "my_connection" {
@@ -35,7 +35,7 @@ resource "google_cloudbuildv2_connection" "my_connection" {
     github_config {
         app_installation_id = 55361269
         authorizer_credential {
-            oauth_token_secret_version = google_secret_manager_secret_version.github_token_secret_version.id
+            oauth_token_secret_version = var.secret_data
         }
     }
     depends_on = [google_secret_manager_secret_iam_policy.policy]
