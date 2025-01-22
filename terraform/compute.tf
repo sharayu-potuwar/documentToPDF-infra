dbec705cb186e52da6606de2a20873f439a63b34
Sharayu <sharayu@Sharayus-MacBook-Pro.local>
2025-01-21 23:11:17 -0500

    pubsub integration with service configured

--- terraform/compute.tf
+++ terraform/compute.tf
@@ -1,17 +1,31 @@
-resource "google_cloud_run_service" "cloudrun-srv" {
-  name     = "cloudrun-srv"
+# resource "google_cloud_run_service" "cloudrun-srv" {
+#   name     = "cloudrun-srv"
+#   location = "us-central1"
+
+#   template {
+#     spec {
+#       containers {
+#         image = "us-docker.pkg.dev/cloudrun/container/hello"
+#       }
+#     }
+#   }
+
+#   traffic {
+#     percent         = 100
+#     latest_revision = true
+#   }
+# }
+
+resource "google_cloud_run_v2_service" "default" {
+  name     = "pubsub-doctopdf"
   location = "us-central1"
 
+  deletion_protection = false # set to true to prevent destruction of the resource
+
   template {
-    spec {
-      containers {
-        image = "us-docker.pkg.dev/cloudrun/container/hello"
-      }
+    containers {
+      image = "us-docker.pkg.dev/cloudrun/container/hello" # Replace with newly created image gcr.io/<project_id>/pubsub
     }
   }
-
-  traffic {
-    percent         = 100
-    latest_revision = true
-  }
+  depends_on = [google_project_service.cloudrun_api]
 }
